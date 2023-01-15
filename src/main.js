599ccd65537a9052027bd6daf8b43710c95fd23d
Jaru <daynorcorit@gmail.com>
2023-01-15 16:35:05 -0400

    eliminación de bootstrap

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import '@/assets/bootstrap.min.css'
-import '@/assets/bootstrap.bundle.min.js'
+
 
 createApp(App).use(store).use(router).mount('#app')
