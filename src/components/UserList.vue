bf8e2fbeb75d583c9c1e1f0b31b88ac2a6824e3b
Elmer Mendoza <defreddyelmer@gmail.com>
2023-01-19 22:38:08 -0400

    Cambio de etiquetas

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -54,11 +54,11 @@
                 </v-dialog>
                 <v-dialog v-model="dialogDelete" max-width="500px">
                     <v-card>
-                        <v-card-title class="text-h5">¿Está seguro de eliminar el registro?</v-card-title>
+                        <v-card-title class="text-h5">¿Está seguro de eliminar el usuario?</v-card-title>
                         <v-card-actions>
                             <v-spacer></v-spacer>
-                            <v-btn color="blue-darken-1" variant="text" @click="closeDelete">Cancelar</v-btn>
-                            <v-btn color="blue-darken-1" variant="text" @click="deleteItemConfirm">Borrar</v-btn>
+                            <v-btn color="blue-darken-1" variant="text" @click="closeDelete">No</v-btn>
+                            <v-btn color="blue-darken-1" variant="text" @click="deleteItemConfirm">Si</v-btn>
                             <v-spacer></v-spacer>
                         </v-card-actions>
                     </v-card>
