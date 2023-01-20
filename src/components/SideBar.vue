773a103368de11ad54aae1b71d511ce85fc08057
Elmer Mendoza <defreddyelmer@gmail.com>
2023-01-19 23:18:29 -0400

    Adición de acciones al menú sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -7,8 +7,27 @@
         <v-btn variant="text" icon="mdi-filter"></v-btn>
         <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
     </v-app-bar>
+
     <v-navigation-drawer v-model="drawer" location="left">
-        <v-list :items="items"></v-list>
+        <v-list density="compact">
+            <v-list-subheader>ADMINISTRACIÓN</v-list-subheader>
+
+            <v-list-item v-for="(item, i) in items" :key="i" :value="item" active-color="primary" :to="item.path">
+                <template v-slot:prepend>
+                    <v-icon :icon="item.icon"></v-icon>
+                </template>
+
+                <v-list-item-title v-text="item.title"></v-list-item-title>
+            </v-list-item>
+        </v-list>
+
+        <template v-slot:append>
+            <div class="pa-2">
+                <v-btn block>
+                    Cerrar sesión
+                </v-btn>
+            </div>
+        </template>
     </v-navigation-drawer>
 </template>
 
@@ -18,14 +37,8 @@ export default {
         drawer: true,
         group: null,
         items: [
-            {
-                title: 'Usuarios',
-                value: 'usuarios',
-            },
-            {
-                title: 'Entidades',
-                value: 'entidades',
-            },
+            { title: 'Home', icon: 'mdi-view-dashboard', path: '/'},
+            { title: 'Usuarios', icon: 'mdi-account-box', path: '/users' },
         ],
     }),
 
