b41ef17f38c4a85b6141a34468759b55fbeabaea
Jaru <daynorcorit@gmail.com>
2023-01-26 19:24:27 -0400

    se reformo el recuperar cpntraseña

--- src/views/RecoverView.vue
+++ src/views/RecoverView.vue
@@ -1,61 +1,51 @@
 <template>
-     
-     <v-app>
-        <v-form ref="form">
-            <h4>Recuperar Contraseña</h4>
-            <br>
-            <div class="login">
-                <v-text-field label="Usuario" :counter="10" :rules="nameContraseña" required  prepend-icon=" mdi-account"></v-text-field>
-            </div>
-                <v-btn depressed color="red" class="botton" >Ingresar</v-btn>
-        </v-form>       
+    <v-app>
+        <div class="background"></div>  
+        <v-main class="d-flex justify-center align-center">
+            <v-col cols="4" lg="4" class="mx-auto">
+                <v-card class="pa-5">
+                    <div class="text-center">
+                        <v-car >
+                          <h2>Recuperar contraseña</h2>
+                        </v-car>
+                    </div>
+                    <v-form>
+                        <v-card-text>
+                            <v-text-field
+                                type="username"
+                                label="Usuario"
+                                placeholder="Username"
+                                prepend-inner-icon="mdi-account"
+                            />
+                        </v-card-text>
+                        <v-card-actions  class="justify-center">
+                        <v-btn
+                         color="primary"
+                         :disabled="!form"
+                         variant="elevated"
+                        >
+                         Enviar
+                        </v-btn>
+
+                        </v-card-actions>
+                    </v-form>
+                </v-card>
+            </v-col>
+        </v-main> 
     </v-app>
 </template>
 <script>
-    export default{
-        data(){
-            return{
-                valida:true,
-                contraseña:'',
-                repiteContraseña:'',
-                show:false,
-                nameContraseña:[
-                    v => !!v || 'Nombre de Usuario Requerido'
-                ],
-                RepiteContraseña:[
-                    v => !!v || 'Repite la contraseña'
-                ]
-            }
-        }
-    }
+export default{
+    name:'App',
+
+    data:()=>({
+        passwordShow:false,
+            
+        }),
+};
+
+
 </script>
 <style>
-form {
-    margin: 11rem auto;
-    width: 30%;
-    min-width: 35%;
-    max-width: 100%;
-    background: rgba(245, 235, 220);
-    border-radius: 20px;
-    padding: 50px;
-   
-}
-h4{
-    text-align: center;
-    color: #502314;
-
-}
-.botton{
-    top:5% ;
-    left: 35%;
-    right: 5%;
-    bottom: 60%;
-    padding-right:5%;
-    color:white;
-  
-}
-.login{
-    color: rgba(80, 35, 20, 0.6);
-}
-
+ 
 </style>
