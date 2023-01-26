c36fdc0325a6f80136035bdf6504a35d0ad36781
Elmer Mendoza <defreddyelmer@gmail.com>
2023-01-25 21:38:25 -0400

    Adición de FooterBar para mostrar el nombre de usuario

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <v-app>
     <SideBar />
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
+    <FooterBar />
   </v-app>
 </template>
 
 <script setup>
 import SideBar from "./components/SideBar.vue";
+import FooterBar from "./components/FooterBar.vue";
 </script>
 
---
+++ src/components/FooterBar.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-footer border app>
+    <div class="px-4 py-2 text-right w-100">
+      <v-icon size="small" class="me-2">
+        mdi-account-circle
+      </v-icon>
+      <span>Usuario</span>
+    </div>
+  </v-footer>
+</template>
